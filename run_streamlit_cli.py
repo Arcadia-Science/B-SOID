f39d63a36ce4d8c6b3746115a110be27ce824cca
Emily Weiss <emily.weiss@arcadiascience.com>
2024-02-12 16:23:42 -0800

    Update run_streamlit_cli.py
    Signed-off-by: Emily Weiss <emily.weiss@arcadiascience.com>

--- run_streamlit_cli.py
+++ run_streamlit_cli.py
@@ -3,39 +3,19 @@ import subprocess
 import os
 import argparse
 
-# Create an argument parser
-parser = argparse.ArgumentParser(description='Run a Streamlit app with a specified working directory.')
-
-# Add an argument for the working directory
-parser.add_argument('--working_dir', type=str, help='Path to where you want outputs to go')
-parser.add_argument('--prefix', type=str, help='Prefix that you want appended to output files')
-parser.add_argument('--software_choice', type=str, help="Pose estimation software; must be 'DeepLabCut','SLEAP', or 'OpenPose'")
-parser.add_argument('--ftype', type=str, help="File type; DeepLabCut: 'h5' or 'csv', SLEAP: 'h5', and OpenPose: 'json'")
-parser.add_argument('--root_path', type=str, help='Path to the working directory containing sub-directories that have input .csv,.h5, or .json files')
-parser.add_argument('--framerate', type=float, help='Framerate for pose estimate files; in frames per second')
-parser.add_argument('--value', type=float, help='Number of sub-directories that have input .csv,.h5, or .json files')
-parser.add_argument('--data_directories', type=str, help="Path of sub-directories that have input .csv,.h5, or .json files; provided as a list and relative to working_dir, e.g. '/1_1,/1_2,/2_1,/2_2'")
-parser.add_argument('--min_cluster_range', type=str, default='0.5', help='Minimum cluster size, based on minimum temporal bout and will represent a %. Impacts number of clusters')
-parser.add_argument('--max_cluster_range', type=str, default='1', help='Maximum cluster size, will represent a % and impacts number of clusters')
-parser.add_argument('--autosave', type=str, default='Yes', help="Whether or not you want to autosave clustering as you go. Should be 'Yes' or 'No'")
-parser.add_argument('--pose_list', type=str, help="List of poses to include in analysis, for example, 'R_rear,L_rear'")
-
-# Parse the command line arguments
-args = parser.parse_args()
-
-# Set environment variables
-os.environ['working_dir'] = args.working_dir
-os.environ['prefix'] = args.prefix
-os.environ['software_choice'] = args.software_choice
-os.environ['ftype'] = args.ftype
-os.environ['root_path'] = args.root_path
-os.environ['framerate'] = str(args.framerate)
-os.environ['value'] = str(args.value)
-os.environ['data_directories'] = args.data_directories
-os.environ['min_cluster_range'] = args.min_cluster_range
-os.environ['max_cluster_range'] = args.max_cluster_range
-os.environ['autosave'] = args.autosave
-os.environ['pose_list'] = args.pose_list
+# Descriptions of environment variables that need to be specified to run the CLI command
+# WORKING_DIR_BSOID: Path to where you want outputs to go.
+# PREFIX_BSOID: Prefix that you want appended to output files.
+# SOFTWARE_BSOID: Pose estimation software; must be 'DeepLabCut','SLEAP', or 'OpenPose'.
+# FTYPE_BSOID: File type; DeepLabCut: 'h5' or 'csv', SLEAP: 'h5', and OpenPose: 'json'.
+# ROOT_PATH_BSOID: Path to the working directory containing sub-directories that have input .csv,.h5, or .json files.
+# FRAMERATE_BSOID: Framerate for pose estimate files; in frames per second.
+# VALUE_BSOID: Number of sub-directories that have input .csv,.h5, or .json files.
+# DATA_DIR_BSOID: Path of sub-directories that have input .csv,.h5, or .json files; provided as a list and relative to working_dir, e.g. '/1_1,/1_2,/2_1,/2_2'.
+# MIN_CLUSTER_BSOID: default='0.5',Minimum cluster size, based on minimum temporal bout and will represent a %. Impacts number of clusters.
+# MAX_CLUSTER_BSOID: default='1', Maximum cluster size, will represent a % and impacts number of clusters.
+# AUTOSAVE_BSOID: Whether or not you want to autosave clustering as you go. Should be 'Yes' or 'No'.
+# POSE_LIST_BSOID: List of poses to include in analysis, for example, 'R_rear,L_rear'.
 
 # Define the command to run the Streamlit app
 cmd = [
