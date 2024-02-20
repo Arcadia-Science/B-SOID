46fb970741607f10b3edf204e983f31cad91600f
Emily Weiss <emily.weiss@arcadiascience.com>
2024-02-20 11:25:35 -0800

    Update bsoid_app.py
    Signed-off-by: Emily Weiss <emily.weiss@arcadiascience.com>

--- bsoid_app.py
+++ bsoid_app.py
@@ -8,27 +8,27 @@ from bsoid_app.bsoid_utilities import visuals
 from bsoid_app.bsoid_utilities.load_css import local_css
 from bsoid_app.bsoid_utilities.load_workspace import *
 
-def get_env_variable(var_name, default_value=None):
-    #Retrieve an environment variable. Ensure it is not empty if no default value is provided.
-    value = os.environ.get(var_name, default_value)
-    if default_value is None and not value:
-        raise ValueError(f"Environment variable '{var_name}' is required and cannot be empty.")
-    return value
+def get_env_variable(var_name, default_VALUE=None):
+    #Retrieve an environment variable. Ensure it is not empty if no default VALUE is provided.
+    VALUE = os.environ.get(var_name, default_VALUE)
+    if default_VALUE is None and not VALUE:
+        raise VALUEError(f"Environment variable '{var_name}' is required and cannot be empty.")
+    return VALUE
 
 try:
-    working_dir = get_env_variable('WORKING_DIR_BSOID')
-    prefix = get_env_variable('PREFIX_BSOID')
-    framerate = float(get_env_variable('FRAMERATE_BSOID'))
-    software_choice = get_env_variable('SOFTWARE_BSOID')
-    ftype = get_env_variable('FTYPE_BSOID')
-    root_path = get_env_variable('ROOT_PATH_BSOID')
-    value = get_env_variable('VALUE_BSOID')
-    data_directories = get_env_variable('DATA_DIR_BSOID')
-    min_cluster_range = float(get_env_variable('MIN_CLUSTER_BSOID', '0.5'))
-    max_cluster_range = float(get_env_variable('MAX_CLUSTER_BSOID', '1'))
-    autosave = get_env_variable('AUTOSAVE_BSOID', 'Yes')
-    pose_list = get_env_variable('POSE_LIST_BSOID')
-except ValueError as e:
+    WORKING_DIR = get_env_variable('WORKING_DIR_BSOID')
+    PREFIX = get_env_variable('PREFIX_BSOID')
+    FRAMERATE = float(get_env_variable('FRAMERATE_BSOID'))
+    SOFTWARE_CHOICE = get_env_variable('SOFTWARE_BSOID')
+    FTYPE = get_env_variable('FTYPE_BSOID')
+    ROOT_PATH = get_env_variable('ROOT_PATH_BSOID')
+    VALUE = get_env_variable('VALUE_BSOID')
+    DATA_DIRECTORIES = get_env_variable('DATA_DIR_BSOID')
+    MIN_CLUSTER_RANGE = float(get_env_variable('MIN_CLUSTER_BSOID', '0.5'))
+    MAX_CLUSTER_RANGE = float(get_env_variable('MAX_CLUSTER_BSOID', '1'))
+    AUTOSAVE = get_env_variable('AUTOSAVE_BSOID', 'Yes')
+    POSE_LIST = get_env_variable('POSE_LIST_BSOID')
+except VALUEError as e:
     print(e)
     exit(1)  # Exit if any required variable is missing or if a conversion to float fails
 
@@ -41,41 +41,41 @@ st.markdown(title, unsafe_allow_html=True)
 st.text('')
 
 
-processor = data_preprocess.preprocess(working_dir,prefix,software_choice,ftype,root_path,framerate,data_directories,pose_list,value)
+processor = data_preprocess.preprocess(WORKING_DIR,PREFIX,SOFTWARE_CHOICE,FTYPE,ROOT_PATH,FRAMERATE,DATA_DIRECTORIES,POSE_LIST,VALUE)
 processor.compile_data()
 
-[_, _, framerate, _, _, _, processed_input_data, _] = load_data(working_dir, prefix)
-extractor = extract_features.extract(working_dir, prefix, processed_input_data, framerate)
+[_, _, FRAMERATE, _, _, _, processed_input_data, _] = load_data(WORKING_DIR, PREFIX)
+extractor = extract_features.extract(WORKING_DIR, PREFIX, processed_input_data, FRAMERATE)
 extractor.main()
 
-[_, sampled_embeddings] = load_embeddings(working_dir, prefix)
-clusterer = clustering.cluster(working_dir, prefix, sampled_embeddings,autosave, min_cluster_range,max_cluster_range)
+[_, sampled_embeddings] = load_embeddings(WORKING_DIR, PREFIX)
+clusterer = clustering.cluster(WORKING_DIR, PREFIX, sampled_embeddings,AUTOSAVE, MIN_CLUSTER_RANGE,MAX_CLUSTER_RANGE)
 clusterer.main()
 
-[sampled_features, _] = load_embeddings(working_dir, prefix)
-[_, assignments, assign_prob, soft_assignments] = load_clusters(working_dir, prefix)
-exporter = export_training.export(working_dir, prefix, sampled_features,
+[sampled_features, _] = load_embeddings(WORKING_DIR, PREFIX)
+[_, assignments, assign_prob, soft_assignments] = load_clusters(WORKING_DIR, PREFIX)
+exporter = export_training.export(WORKING_DIR, PREFIX, sampled_features,
                                   assignments, assign_prob, soft_assignments)
 exporter.save_csv()
 
-[features, _] = load_feats(working_dir, prefix)
-[sampled_features, _] = load_embeddings(working_dir, prefix)
-[_, assignments, _, _] = load_clusters(working_dir, prefix)
-learning_protocol = machine_learner.protocol(working_dir, prefix, features, sampled_features, assignments)
+[features, _] = load_feats(WORKING_DIR, PREFIX)
+[sampled_features, _] = load_embeddings(WORKING_DIR, PREFIX)
+[_, assignments, _, _] = load_clusters(WORKING_DIR, PREFIX)
+learning_protocol = machine_learner.protocol(WORKING_DIR, PREFIX, features, sampled_features, assignments)
 learning_protocol.main()
 
-[root_path, data_directories, framerate, pose_chosen, input_filenames, _, processed_input_data, _] \
-    = load_data(working_dir, prefix)
-[_, _, _, clf, _, _] = load_classifier(working_dir, prefix)
-creator = video_creator.creator(root_path, data_directories, processed_input_data, pose_chosen,
-                                working_dir, prefix, framerate, clf, input_filenames)
+[ROOT_PATH, DATA_DIRECTORIES, FRAMERATE, pose_chosen, input_filenames, _, processed_input_data, _] \
+    = load_data(WORKING_DIR, PREFIX)
+[_, _, _, clf, _, _] = load_classifier(WORKING_DIR, PREFIX)
+creator = video_creator.creator(ROOT_PATH, DATA_DIRECTORIES, processed_input_data, pose_chosen,
+                                WORKING_DIR, PREFIX, FRAMERATE, clf, input_filenames)
 creator.main()
 
-[root_path, data_directories, framerate, pose_chosen, input_filenames, _, processed_input_data, _] \
-    = load_data(working_dir, prefix)
-[_, _, _, clf, _, predictions] = load_classifier(working_dir, prefix)
-predictor = predict.prediction(root_path, data_directories, input_filenames, processed_input_data, working_dir,
-                               prefix, framerate, pose_chosen, predictions, clf)
+[ROOT_PATH, DATA_DIRECTORIES, FRAMERATE, pose_chosen, input_filenames, _, processed_input_data, _] \
+    = load_data(WORKING_DIR, PREFIX)
+[_, _, _, clf, _, predictions] = load_classifier(WORKING_DIR, PREFIX)
+predictor = predict.prediction(ROOT_PATH, DATA_DIRECTORIES, input_filenames, processed_input_data, WORKING_DIR,
+                               PREFIX, FRAMERATE, pose_chosen, predictions, clf)
 predictor.main()
 
 streamlit_run('./bsoid_app/bsoid_analysis')
