8b11fbf4a157942b500209f50195255c1615a520
Emily Weiss <emily.weiss@arcadiascience.com>
2024-02-21 12:13:17 -0800

    Update bsoid_app.py
    changing VALUE to value
    
    Signed-off-by: Emily Weiss <emily.weiss@arcadiascience.com>

--- bsoid_app.py
+++ bsoid_app.py
@@ -9,11 +9,11 @@ from bsoid_app.bsoid_utilities.load_css import local_css
 from bsoid_app.bsoid_utilities.load_workspace import *
 
 def get_env_variable(var_name, default_value=None):
-    # Retrieve an environment variable. Ensure it is not empty if no default VALUE is provided.
-    VALUE = os.environ.get(var_name, default_VALUE)
-    if default_VALUE is None and not VALUE:
+    # Retrieve an environment variable. Ensure it is not empty if no default value is provided.
+    value = os.environ.get(var_name, default_value)
+    if default_value is None and not value:
         raise ValueError(f"Environment variable '{var_name}' is required and cannot be empty.")
-    return VALUE
+    return value
 
 try:
     WORKING_DIR = get_env_variable('WORKING_DIR_BSOID')
