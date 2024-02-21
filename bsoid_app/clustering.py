fddbb8ede58ba1c5b93485ae7048ecb87490ac31
Emily Weiss <emily.weiss@arcadiascience.com>
2024-02-21 10:40:15 -0800

    Update clustering.py
    Signed-off-by: Emily Weiss <emily.weiss@arcadiascience.com>

--- bsoid_app/clustering.py
+++ bsoid_app/clustering.py
@@ -13,16 +13,16 @@ from streamlit import caching
 
 class cluster:
 
-    def __init__(self, working_dir, prefix, sampled_embeddings, autosave, min_cluster_range, max_cluster_range):
+    def __init__(self, WORKING_DIR, PREFIX, sampled_embeddings, AUTOSAVE, MIN_CLUSTER_RANGE, MAX_CLUSTER_RANGE):
         print('IDENTIFY AND TWEAK NUMBER OF CLUSTERS.')
-        self.working_dir = working_dir
-        self.prefix = prefix
+        self.WORKING_DIR = WORKING_DIR
+        self.PREFIX = PREFIX
         self.sampled_embeddings = sampled_embeddings
         self.cluster_range = []
-        self.autosave = autosave
-        self.min_cluster_range = min_cluster_range
-        self.max_cluster_range = max_cluster_range
-        self.cluster_range = [min_cluster_range, max_cluster_range]
+        self.AUTOSAVE = AUTOSAVE
+        self.MIN_CLUSTER_RANGE = MIN_CLUSTER_RANGE
+        self.MAX_CLUSTER_RANGE = MAX_CLUSTER_RANGE
+        self.cluster_range = [MIN_CLUSTER_RANGE, MAX_CLUSTER_RANGE]
         print(f'Your cluster range is set to {self.cluster_range}')
         self.assignments = []
         self.assign_prob = []
@@ -62,16 +62,16 @@ class cluster:
 
 
     def save(self):
-        if self.autosave == 'Yes':
-            print('You have chosen to autosave clustering as you go (overwrites previously saved clustering).')
-            with open(os.path.join(self.working_dir, str.join('', (self.prefix, '_clusters.sav'))), 'wb') as f:
+        if self.AUTOSAVE == 'Yes':
+            print('You have chosen to AUTOSAVE clustering as you go (overwrites previously saved clustering).')
+            with open(os.path.join(self.WORKING_DIR, str.join('', (self.PREFIX, '_clusters.sav'))), 'wb') as f:
                 joblib.dump([self.min_cluster_size, self.assignments, self.assign_prob, self.soft_assignments], f)
 
     def main(self):
         try:
             caching.clear_cache()
             [self.min_cluster_size, self.assignments, self.assign_prob, self.soft_assignments] = \
-                load_clusters(self.working_dir, self.prefix)
+                load_clusters(self.WORKING_DIR, self.PREFIX)
             print(
                 'Done assigning labels for {} instances in {} D space. Move on to create '
                 'a model.'.format(self.assignments.shape, self.sampled_embeddings.shape[1]))
