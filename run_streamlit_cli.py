632f488a8f531b1446648f6d08a21274da96ad95
Emily Weiss <emily.weiss@arcadiascience.com>
2024-03-20 19:02:24 -0700

    edited to add a trigger to stop running streamlit/exit
    Signed-off-by: Emily Weiss <emily.weiss@arcadiascience.com>

--- run_streamlit_cli.py
+++ run_streamlit_cli.py
@@ -1,7 +1,6 @@
-import sys
 import subprocess
 import os
-import argparse
+import time
 
 # Descriptions of environment variables that need to be specified to run the CLI command
 # WORKING_DIR_BSOID: Path to where you want outputs to go.
@@ -25,14 +24,24 @@ import argparse
 # SOFTWARE_BSOID='DeepLabCut' FTYPE_BSOID='csv' ROOT_PATH_BSOID='/Users/Desktop/training/' FRAMERATE_BSOID=120 WORKING_DIR_BSOID='/Users/Desktop/training/output' PREFIX_BSOID='controltry' VALUE_BSOID=4.0 DATA_DIR_BSOID='/1_1,/1_2,/2_1,/2_2' AUTOSAVE_BSOID='Yes' POSE_LIST_BSOID='R_rear,L_rear' python run_streamlit_cli.py
 
 
-# Define the command to run the Streamlit app
-def main():
-    cmd = [
-        "streamlit",
-        "run",
-        "bsoid_app.py",
-    ]
-    subprocess.run(cmd)
+def run_streamlit():
+    signal_file = 'app_done.txt'
+    if os.path.exists(signal_file):
+        os.remove(signal_file)
+
+    process = subprocess.Popen(["streamlit", "run", "bsoid_app.py"])
+
+    while not os.path.exists(signal_file):
+        time.sleep(1)
+
+    process.terminate()
+    try:
+        process.wait(timeout=10)
+    except subprocess.TimeoutExpired:
+        process.kill()
+
+    if os.path.exists(signal_file):
+        os.remove(signal_file)
 
 if __name__ == "__main__":
-    main()
+    run_streamlit()
